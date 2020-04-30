347888c44af1bf6cf0ffaa1a283280db51240d9c
BrandonLN <brandonln@gmail.com>
2020-04-30 06:50:50 -0600

    mods to burger names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royal Red Robin Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Reds Tavern Double", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole Bacon Burger", FALSE);
