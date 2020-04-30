db64a9b13846a4309b63d49c477b4e7381bf5421
BrandonLN <brandonln@gmail.com>
2020-04-30 07:25:27 -0600

    models -> burger.js built out

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Royal Red Robin Burger", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Reds Tavern Double", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole Bacon Burger", FALSE);
+INSERT INTO burgers (burger_name, ravaged) VALUES ("Royal Red Robin Burger", FALSE);
+INSERT INTO burgers (burger_name, ravaged) VALUES ("Reds Tavern Double", FALSE);
+INSERT INTO burgers (burger_name, ravaged) VALUES ("Guacamole Bacon Burger", FALSE);
